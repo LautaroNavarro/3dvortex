0032a6546a048af3ca570a9068702c7c28f7c2c4
LautaroNavarro <navarro_lautaro@hotmail.com>
2019-09-10 20:48:47 -0300

    Nullable fields on user model

--- src/users/models/user.py
+++ src/users/models/user.py
@@ -3,10 +3,22 @@ from django.db import models
 
 class User(models.Model):
 
-    name = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
+    name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+    lastname = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    mercado_pago_id = models.CharField(max_length=255)
+    mercado_pago_id = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
     created = models.DateTimeField()
     changed = models.DateTimeField()
