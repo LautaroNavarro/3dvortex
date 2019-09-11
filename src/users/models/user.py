b89c7ce0d0693e6ae863ce48a5a7c55a2ebf94ec
LautaroNavarro <navarro_lautaro@hotmail.com>
2019-09-10 21:07:53 -0300

    User email field to EmailField

--- src/users/models/user.py
+++ src/users/models/user.py
@@ -16,7 +16,7 @@ class User(models.Model):
         blank=True,
         null=True,
     )
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
     status = models.IntegerField(default=NOT_CONFIRMED_STATUS)
     password = models.CharField(max_length=255)
     mercado_pago_id = models.CharField(
